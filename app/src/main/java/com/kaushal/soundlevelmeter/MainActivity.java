fea922c83bc5614b2648b449d68954a35e5873ba
kdontula <kdontula@hawk.iit.edu>
2016-07-28 18:24:09 -0500

    alert box for settings

--- app/src/main/java/com/kaushal/soundlevelmeter/MainActivity.java
+++ app/src/main/java/com/kaushal/soundlevelmeter/MainActivity.java
@@ -1,13 +1,21 @@
 package com.kaushal.soundlevelmeter;
 
+/**
+ * Created by xkxd061 on 7/26/16.
+ */
+
 import android.app.Activity;
-import android.content.Intent;
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -17,68 +25,56 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.text.DecimalFormat;
 
-public class MainActivity extends Activity implements
-        AudioInputListener {
+public class MainActivity extends Activity implements AudioInputListener {
+    private final Handler handler = new Handler();
+    Runnable runTimer;
     AudioInput micInput;
-    TextView mdBTextView;
-    TextView mdBFractionTextView;
-    BarLevelDrawable mBarLevel;
-    private TextView mGainTextView;
-
-    double mOffsetdB = 10;
-    double mGain = 2500.0 / Math.pow(10.0, 90.0 / 20.0);
-    double mDifferenceFromNominal = 0.0;
-    double mRmsSmoothed;
+    TextView primaryDb;
+    TextView fractionDb;
+    private TextView gainDb;
+    private LineGraphSeries<DataPoint> graphData;
+    private double graphLastXValue = 5d;
+    double gain = 2500.0 / Math.pow(10.0, 90.0 / 20.0);
+    double differenceFromNominal = 0.0;
+    double smoothedRms;
     double mAlpha = 0.9;
-    private int mSampleRate;
-    private int mAudioSource;
-    private volatile boolean mDrawing;
-    private volatile int mDrawingCollided;
-
-    private static final String TAG = "LevelMeterActivity";
+    private int sampleRate;
+    private int audioSource;
+    private volatile boolean drawing;
+    private volatile int drawingCollided;
+    GraphView graph;
+    Dialog settingsDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         micInput = new AudioInput(this);
-
         setContentView(R.layout.activity_main);
-
-        mBarLevel = (BarLevelDrawable) findViewById(R.id.bar_level_drawable_view);
-        mdBTextView = (TextView) findViewById(R.id.dBTextView);
-        mdBFractionTextView = (TextView) findViewById(R.id.dBFractionTextView);
-        mGainTextView = (TextView) findViewById(R.id.gain);
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
-
-
-        final ToggleButton onOffButton = (ToggleButton) findViewById(
-                R.id.on_off_toggle_button);
-
+        primaryDb = (TextView) findViewById(R.id.dBTextView);
+        fractionDb = (TextView) findViewById(R.id.dBFractionTextView);
+        gainDb = (TextView) findViewById(R.id.gain);
+        graph = (GraphView) findViewById(R.id.graph);
+        graphData = new LineGraphSeries<DataPoint>();
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(20);
+        final ToggleButton onOffButton = (ToggleButton) findViewById(R.id.on_off_toggle_button);
         ToggleButton.OnClickListener tbListener =
                 new ToggleButton.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (onOffButton.isChecked()) {
                             readPreferences();
-                            micInput.setSampleRate(mSampleRate);
-                            micInput.setAudioSource(mAudioSource);
+                            micInput.setSampleRate(sampleRate);
+                            micInput.setAudioSource(audioSource);
                             micInput.start();
+                            graph.addSeries(graphData);
                         } else {
                             micInput.stop();
                         }
                     }
                 };
         onOffButton.setOnClickListener(tbListener);
-
         Button minus5dbButton = (Button) findViewById(R.id.minus_5_db_button);
         DbClickListener minus5dBButtonListener = new DbClickListener(-5.0);
         minus5dbButton.setOnClickListener(minus5dBButtonListener);
@@ -91,23 +87,45 @@ public class MainActivity extends Activity implements
         Button plus5dbButton = (Button) findViewById(R.id.plus_5_db_button);
         DbClickListener plus5dBButtonListener = new DbClickListener(5.0);
         plus5dbButton.setOnClickListener(plus5dBButtonListener);
-        Button settingsButton = (Button) findViewById(R.id.settingsButton);
-        Button.OnClickListener settingsBtnListener =
-                new Button.OnClickListener() {
+        final Button settingsButton = (Button) findViewById(R.id.settingsButton);
+        Button.OnClickListener settingsBtnListener = new Button.OnClickListener() {
+            public void onClick(View v) {
+                onOffButton.setChecked(false);
+                MainActivity.this.micInput.stop();
+                showSettingsDialog();
+            }
 
+        };
+        settingsButton.setOnClickListener(settingsBtnListener);
+    }
+
+    private void showSettingsDialog() {
+        settingsDialog = new Dialog(MainActivity.this);
+        settingsDialog.setContentView(R.layout.sample_settings);
+        settingsDialog.setTitle("Settings:");
+        Spinner spinner = (Spinner) settingsDialog.findViewById(R.id.spinnerSampleRate);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                MainActivity.this, R.array.sample_rate_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        spinner.setPrompt(Integer.toString(sampleRate));
+        int spinnerPosition = adapter.getPosition(Integer.toString(sampleRate));
+        spinner.setSelection(spinnerPosition);
+
+
+        Button okButton = (Button) settingsDialog.findViewById(R.id.settingsOkButton);
+        Button.OnClickListener okBtnListener =
+                new Button.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        final ToggleButton onOffButton = (ToggleButton) findViewById(
-                                R.id.on_off_toggle_button);
-                        onOffButton.setChecked(false);
-                        MainActivity.this.micInput.stop();
+                        MainActivity.this.setPreferences();
+                        settingsDialog.dismiss();
 
-                        Intent settingsIntent = new Intent(MainActivity.this,
-                                SampleOptions.class);
-                        MainActivity.this.startActivity(settingsIntent);
                     }
                 };
-        settingsButton.setOnClickListener(settingsBtnListener);
+        okButton.setOnClickListener(okBtnListener);
+        settingsDialog.show();
     }
 
     private class DbClickListener implements Button.OnClickListener {
@@ -119,53 +137,76 @@ public class MainActivity extends Activity implements
 
         @Override
         public void onClick(View v) {
-            MainActivity.this.mGain *= Math.pow(10, gainIncrement / 20.0);
-            mDifferenceFromNominal -= gainIncrement;
+            MainActivity.this.gain *= Math.pow(10, gainIncrement / 20.0);
+            differenceFromNominal -= gainIncrement;
             DecimalFormat df = new DecimalFormat("##.# dB");
-            mGainTextView.setText(df.format(mDifferenceFromNominal));
+            gainDb.setText(df.format(differenceFromNominal));
         }
     }
 
     private void readPreferences() {
-        SharedPreferences preferences = getSharedPreferences("LevelMeter",
-                MODE_PRIVATE);
-        mSampleRate = preferences.getInt("SampleRate", 8000);
-        mAudioSource = preferences.getInt("AudioSource",
-                MediaRecorder.AudioSource.VOICE_RECOGNITION);
+        SharedPreferences preferences = getSharedPreferences("SoundLevelMeter", MODE_PRIVATE);
+        sampleRate = preferences.getInt("SampleRate", 8000);
+        audioSource = preferences.getInt("AudioSource", MediaRecorder.AudioSource.VOICE_RECOGNITION);
     }
 
     @Override
     public void processAudioFrame(short[] audioFrame) {
-        if (!mDrawing) {
-            mDrawing = true;
-            double rms = 0;
-            for (int i = 0; i < audioFrame.length; i++) {
-                rms += audioFrame[i] * audioFrame[i];
-            }
-            rms = Math.sqrt(rms / audioFrame.length);
-
-            mRmsSmoothed = mRmsSmoothed * mAlpha + (1 - mAlpha) * rms;
-            final double rmsdB = 20.0 * Math.log10(mGain * mRmsSmoothed);
-
-
-            mBarLevel.post(new Runnable() {
+        if (!drawing) {
+            drawing = true;
+            final double rmsdB = getRmsdB(audioFrame);
+            runTimer = new Runnable() {
                 @Override
                 public void run() {
-                    mBarLevel.setLevel((mOffsetdB + rmsdB) / 60);
-
+                    graphLastXValue += 1d;
+                    graphData.appendData(new DataPoint(graphLastXValue, 20 + rmsdB), true, 40);
                     DecimalFormat df = new DecimalFormat("##");
-                    mdBTextView.setText(df.format(20 + rmsdB));
-
-                    DecimalFormat df_fraction = new DecimalFormat("#");
+                    primaryDb.setText(df.format(20 + rmsdB));
                     int one_decimal = (int) (Math.round(Math.abs(rmsdB * 10))) % 10;
-                    mdBFractionTextView.setText(Integer.toString(one_decimal));
-                    mDrawing = false;
+                    fractionDb.setText(Integer.toString(one_decimal));
+                    drawing = false;
                 }
-            });
+            };
+            handler.postDelayed(runTimer, 100);
         } else {
-            mDrawingCollided++;
-            Log.v(TAG, "Level bar update collision, i.e. update took longer " +
-                    "than 20ms. Collision count" + Double.toString(mDrawingCollided));
+            drawingCollided++;
+            Log.v("LevelMeterActivity", "Level bar update collision, i.e. update took longer " +
+                    "than 20ms. Collision count" + Double.toString(drawingCollided));
         }
+    }
+
+    private double getRmsdB(short[] audioFrame) {
+        double rms = 0;
+        for (int i = 0; i < audioFrame.length; i++) {
+            rms += audioFrame[i] * audioFrame[i];
+        }
+        rms = Math.sqrt(rms / audioFrame.length);
+        smoothedRms = smoothedRms * mAlpha + (1 - mAlpha) * rms;
+        return 20.0 * Math.log10(gain * smoothedRms);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public class MyOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            MainActivity.this.sampleRate =
+                    Integer.parseInt(parent.getItemAtPosition(pos).toString());
+        }
+        @Override
+        public void onNothingSelected(AdapterView parent) {
+            // Do nothing.
+        }
+    }
+
+    private void setPreferences() {
+        SharedPreferences preferences = getSharedPreferences("SoundLevelMeter",
+                MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("SampleRate", sampleRate);
+        editor.apply();
     }
 }
